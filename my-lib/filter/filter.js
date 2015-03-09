6b0d68bdace49c3de4a70bfd1ae6e43c4c9c93b4
joel <iamjoel007@gmail.com>
2015-03-09 10:29:35 +0800

    add get search str example

--- my-lib/filter/filter.js
+++ my-lib/filter/filter.js
@@ -9,8 +9,51 @@
         init: function() {
 
         },
-        getSerchStr: function() {
+        getSearchStr: function() {
+            // example
+            // var queryArr = [];
+            // var $el = this.$el;
+            // if ($el) {
+            //     var $querys = $el.find('.search-condition .condition-item');
+            //     queryArr.push([this.pageParamName, this.pageAt].join('='));
+            //     $querys.each(function() {
+            //         var $this = $(this);
+            //         var $selected = $this.find('.selected');
+            //         var val = $selected.data('id');
+            //         if (val !== undefined) {
+            //             queryArr.push([$this.data('type-id'), val].join('='));
+            //         }
+            //     });
+            //     var $searchText = $el.find('.search-condition .text-query-item :text');
+            //     $searchText.each(function() {
+            //         var $this = $(this);
+            //         var val = $this.val();
+            //         if ($.trim(val) !== '') {
+            //             queryArr.push([$this.attr('name'), val].join('='));
+            //         }
+            //     });
 
+            //     var $sortBtn = $('.selected.sort-btn', $el);
+            //     $sortBtn.each(function() {
+            //         var $this = $(this);
+            //         queryArr.push([$this.data('type-id'), $this.data('id')].join('='));
+            //     });
+
+            //     var $checkbox = $('.checkbox:checked', $el);
+            //     $checkbox.each(function() {
+            //         var $this = $(this);
+            //         queryArr.push([$this.attr('name'), $this.val()].join('='));
+            //     });
+
+            //     var res = queryArr.join('&');
+            //     var url = this.getUrl();
+            //     if (url.indexOf('?') > -1) {
+            //         res = '&' + res;
+            //     } else {
+            //         res = '?' + res;
+            //     }
+            //     return res;
+            // }
         },
         render: function(data, scope) {
 
@@ -21,7 +64,7 @@
             if (typeof url === 'function') {
                 url = url(this);
             }
-            var searchStr = this.getSerchStr(this);
+            var searchStr = this.getSearchStr(this);
             url = url + searchStr;
             url = encodeURI(url);
             $.ajax({
